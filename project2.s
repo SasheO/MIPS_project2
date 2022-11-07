3e2bac2b67e5315945bfa56c199cd57ed3695de9
SasheO <76496417+SasheO@users.noreply.github.com>
2022-11-07 17:43:13 -0500

    included a zero to nine_check and partly populated it

--- project2.s
+++ project2.s
@@ -21,7 +21,7 @@ convert_string_to_decimal:
 # output registers used: 
 #       $v0 - whether string is invalid (0) or not (non-zero)
 #       $v1 - the convert_string_to_decimal value of string, if valid
-#       temporary registers used: $t0,$t1,$t2,$t3
+#       temporary registers used: $t0,$t1,$t2,$t3,$t4
 #
 #
 # called by main
@@ -38,6 +38,12 @@ loop:
     lb $t0,0($a0) # load character at this of string into $t0
     beq $t0,$zero,exit_subprogram # when null char is read
     addi $a0,$a0,1 # increment the address in $a0 by one to move onto next character in the next loop
+
+    check_0_to_9:
+        slti $t4,$t0,48 # the string char in $t1 should be greater than or equal to '0' char i.e. $t4 should be 0
+        bne $t4,$zero,check_a_to_p # if $t4 not 0, do the next check
+        slti $t4,$t0,58 # check if character <= ascii code for 9 # the string char in $t1 should be less than or equal to '9' char i.e. $t0 should be 1
+        beq $t4,$zero,check_a_to_p # if $t0 0 instead of 1, do the next check
 
 j loop
 
