b40adea101bd0b654228da8308a173aa0fedddd5
SasheO <76496417+SasheO@users.noreply.github.com>
2022-11-07 17:27:33 -0500

    explanatory comments in subprogram

--- project2.s
+++ project2.s
@@ -10,5 +10,10 @@ li $v0,10
 syscall
 
 convert_string_to_decimal:
-
+##################################################
+# input registers used: $a0
+# output registers used: 
+#       $v0 - whether string is invalid or not
+#       $v1 - the convert_string_to_decimal value of string, if valid
+###################################################
 jr $ra
