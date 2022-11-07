30e0bbbbc9f085e0138299de7da7ebf1a92676be
SasheO <76496417+SasheO@users.noreply.github.com>
2022-11-07 17:15:49 -0500

    changed indentations, included unrecognized input .data declarations, declared subprogram

--- project2.s
+++ project2.s
@@ -1,9 +1,14 @@
 .data
-    input_str: .space 1001
+input_str: .space 1001
+unrecognized_input: .asciiz "Unrecognized input"
 
 .text
 main:
 
 
-    li $v0,10
-    syscall
+li $v0,10
+syscall
+
+convert_string_to_decimal:
+
+jr $ra
