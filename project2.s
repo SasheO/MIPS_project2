2ff0e8c170d5341cb9f76f75b87cfe5c1090fb11
SasheO <76496417+SasheO@users.noreply.github.com>
2022-11-07 18:48:13 -0500

    updated comments

--- project2.s
+++ project2.s
@@ -53,7 +53,7 @@ loop:
     addi $a0,$a0,1 # increment the address in $a0 by one to move onto next character in the next loop
 
     check_0_to_9:
-        slti $t4,$t0,48 # the string char in $t1 should be greater than or equal to '0' char i.e. $t4 should be 0
+        slti $t4,$t0,48 # the string char in $t0 should be greater than or equal to '0' char i.e. $t4 should be 0
         bne $t4,$zero,check_a_to_p # if $t4 not 0, do the next check
         slti $t4,$t0,58 # check if character <= ascii code for 9 # the string char in $t0 should be less than or equal to '9' char i.e. $t0 should be 1
         beq $t4,$zero,check_a_to_p # if $t0 0 instead of 1, do the next check
@@ -63,7 +63,7 @@ loop:
 
 
     check_a_to_p:
-        slti $t4,$t0,97 # the string char in $t1 should be greater than or equal to 'a' char i.e. $t0 should be 0
+        slti $t4,$t0,97 # the string char in $t0 should be greater than or equal to 'a' char i.e. $t0 should be 0
         bne $t4,$zero,check_A_to_P # if $t4 not 0, do the next check
         slti $t4,$t0,113 # check if character <= ascii code for 'p' # the string char in $t0 should be less than or equal to 'p' char i.e. $t4 should be 1
         beq $t4,$zero,check_A_to_P # if $t4 0 instead of 1, do the next check
@@ -72,7 +72,7 @@ loop:
         j add_to_running_sum # j to segment of loop that adds char value to value of $v1, the running sum
 
     check_A_to_P:
-        slti $t4,$t0,65 # the string char in $t1 should be greater than or equal to 'A' char i.e. $t4 should be 0
+        slti $t4,$t0,65 # the string char in $t0 should be greater than or equal to 'A' char i.e. $t4 should be 0
         bne $t4,$zero,check_space_char # if $t4 not 0, do the next check
         slti $t4,$t0,81 # check if character <= ascii code for 'P' # the string char in $t0 should be less than or equal to 'p' char i.e. $t4 should be 1
         beq $t4,$zero,check_space_char # if $t4 0 instead of 1, do the next check
