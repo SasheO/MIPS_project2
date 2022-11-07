e37a350fde495eacb209c84a1cc04229315cfb09
SasheO <76496417+SasheO@users.noreply.github.com>
2022-11-07 17:34:23 -0500

    updated subprogram description comment, looped the loop

--- project2.s
+++ project2.s
@@ -21,12 +21,20 @@ convert_string_to_decimal:
 # output registers used: 
 #       $v0 - whether string is invalid or not
 #       $v1 - the convert_string_to_decimal value of string, if valid
+#       temporary registers used: $t0
+#
+# should pass test case of no input string, all spaces input string
+#
+# called by main
+# calls none
 ###################################################
 
 loop:
     lb $t0,0($a0) # load character at this of string into $t0
     beq $t0,$zero,exit_subprogram # when null char is read
     addi $a0,$a0,1 # increment the address in $a0 by one to move onto next character in the next loop
+
+j loop
 
 exit_subprogram:
 jr $ra
