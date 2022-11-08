d865f0157fe9ced2644d98713bfd3e791522b973
SasheO <76496417+SasheO@users.noreply.github.com>
2022-11-07 19:00:59 -0500

    reverted to previous organization

--- project2.s
+++ project2.s
@@ -79,10 +79,6 @@ loop:
 
         addi $t0,$t0,-55 # convert ascii value to integer (A-P ascii: 65-80; A-P here: 10-25)
         j add_to_running_sum # j to segment of loop that adds char value to value of $v1, the running sum
-
-    for_non_valid_inputs:
-        li $v0,0
-        jr $ra
     
     check_space_char:
         # TODO: check if space char, if not it is invalid. branch to for_non_valid_inputs
@@ -94,6 +90,10 @@ loop:
         update_t3_to_one:
             addi $t3,$t3,1
             j loop
+
+    for_non_valid_inputs:
+        li $v0,0
+        jr $ra
 
 add_to_running_sum:
     li $v0,1 # valid chars have been found
